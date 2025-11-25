8d7bbdf97eddef0177af928945bfa360cfd24879
astmain <1311192345@qq.com>
2025-11-25 11:57:49 +0800

    1

--- vuejs1/env.d.ts
+++ vuejs1/env.d.ts
@@ -6,5 +6,15 @@ declare module "vue-drag-resize/src" {
   export default VueDragResize
 }
 
+// 扩展 Element Plus el-table 组件，允许 style 属性接受字符串类型
+import type { CSSProperties } from "vue"
 
+declare module "@vue/runtime-core" {
+  export interface GlobalComponents {
+    ElTable: {
+      style?: string | CSSProperties
+      [key: string]: any
+    } & import("vue").ComponentPublicInstance
+  }
+}
 
